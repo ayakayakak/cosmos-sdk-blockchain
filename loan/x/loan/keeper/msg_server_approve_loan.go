c6e6fcf43d6087c61cc033e13f41105a95648410
taniguchi <bayakau.tka@gmail.com>
2022-07-12 20:39:55 +0900

    add Repay Loan Message

--- loan/x/loan/keeper/msg_server_approve_loan.go
+++ loan/x/loan/keeper/msg_server_approve_loan.go
@@ -3,9 +3,9 @@ package keeper
 import (
 	"context"
 
-	"github.com/username/loan/x/loan/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/username/loan/x/loan/types"
 )
 
 func (k msgServer) ApproveLoan(goCtx context.Context, msg *types.MsgApproveLoan) (*types.MsgApproveLoanResponse, error) {
